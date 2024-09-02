13d6dbf124259a2a20e5d4dca9ebf7c0f8c84258
Minji Kim <minjikim2000@hotmail.com>
2024-09-02 18:34:16 +0900

    docs: 주석 작성

--- project04/src/javascripts/App.js
+++ project04/src/javascripts/App.js
@@ -7,8 +7,11 @@ export default class App {
     this.currentMainIndex = 0;
   }
 
+  // 비동기 초기화 메서드: 앱을 설정하는 작업을 수행
   async setup() {
+    // props에서 el(루트 요소 선택자를) 추출
     const { el } = this.props;
+    // 루트 요소를 DOM에서 선택해 저장
     this.rootElement = document.querySelector(el);
     this.intro = new Intro();
     this.tabButtons = new TabButtons();
@@ -35,7 +38,7 @@ export default class App {
 
   render() {
     removeAllChildNodes(this.rootElement); // 초기화
-    const tabButtons = this.tabButtons.render();
-    this.rootElement.append(tabButtons);
+    const tabButtons = this.tabButtons.render(); // TabButtons를 렌더링하여 버튼들을 가져옴
+    this.rootElement.append(tabButtons); // 루트 요소에 TabButtons를 추가하여 화면에 표시
   }
 }
--- project04/src/javascripts/components/TabButtons/index.js
+++ project04/src/javascripts/components/TabButtons/index.js
@@ -2,19 +2,24 @@ import { findIndexListElement, getClosestElement } from "../../utils/index.js";
 
 export default class TabButtons {
   constructor() {
+    // renderElement는 탭 버튼들을 포함하는 HTML 요소임
     this.renderElement = TabButtons.createRenderElement();
     this.bindEvents();
   }
 
+  // 정적 메서드: 탭 버튼을 생성하는 역할
   static createRenderElement() {
+    // 탭 버튼들을 담을 ul 요소를 생성
     const tabsContainer = document.createElement("ul");
     tabsContainer.classList.add("app-controller");
+    // 탭 목록 정의: 탭의 제목과 아이콘 이름을 설정
     const tabs = [
       { title: "Top5", iconName: "icon-top5" },
       { title: "PlayList", iconName: "icon-playlist" },
       { title: "Search", iconName: "icon-search" },
     ];
 
+    // 탭 버튼들을 li 요소로 생성하고, tabsContainer에 넣음
     tabsContainer.innerHTML = tabs
       .map((tab) => {
         return `
@@ -33,25 +38,32 @@ export default class TabButtons {
 
   bindEvents() {
     this.renderElement.addEventListener("click", (event) => {
-      const element = getClosestElement(event.target, "li"); // 가장 가까운 li를 찾음
+      // 클릭된 요소에서 가장 가까운 li 태그를 찾음
+      const element = getClosestElement(event.target, "li");
+      // 해당 li 요소가 몇 번째인지 인덱스를 찾음
       const currentIndex = findIndexListElement(element);
 
       this.emit("clickTab", { currentIndex });
     });
   }
-  // 통신을 건네주는 함수
-  // app.js은 on함수를 총해 TabButtons 컴포넌트에 뭔가를 요청할 수 있게 함
+  // 통신을 받아 처리하는 함수
+  // 다른 코드가 이 클래스에 이벤트와 콜백 함수를 등록할 수 있게 함
   on(eventName, callback) {
+    // 이벤트 저장소 초기화
     this.events = this.events ? this.events : {};
+    // 특정 이벤트에 대해 콜백 함수를 저장
     this.events[eventName] = callback;
   }
 
-  // 통신을 받는 함수
-  // app.js와 통신할 수 있게 만들어주는 함수
+  // 통신을 보내는 함수
+  // 특정 이벤트가 발생했음을 알리고, 필요한 데이터를 전달
   emit(eventName, payload) {
+    // 해당 이벤트에 연결된 콜백 함수가 있으면 실행
     this.events[eventName] && this.events[eventName](payload);
   }
 
+  // 렌더링 함수
+  // 생성된 HTML 요소를 반환하여 화면에 표시할 수 있게 함
   render() {
     return this.renderElement;
   }
