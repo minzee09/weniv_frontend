7503dd685835ac899abdbb1a02f88c421b314a04
Minji Kim <minjikim2000@hotmail.com>
2024-09-02 19:46:13 +0900

    feat: add TopMusic component

--- project04/src/javascripts/App.js
+++ project04/src/javascripts/App.js
@@ -1,10 +1,12 @@
-import { Intro, TabButtons } from "./components/index.js";
+import { Intro, TabButtons, TopMusic } from "./components/index.js";
+import { fetchMusic } from "../APIs/index.js";
 import removeAllChildNodes from "./utils/removeAllChildNodes.js";
 
 export default class App {
   constructor(props) {
     this.props = props;
     this.currentMainIndex = 0;
+    this.mainViewComponents = [];
   }
 
   // 비동기 초기화 메서드: 앱을 설정하는 작업을 수행
@@ -15,17 +17,40 @@ export default class App {
     this.rootElement = document.querySelector(el);
     this.intro = new Intro();
     this.tabButtons = new TabButtons();
+    this.topMusic = new TopMusic();
+    this.mainViewComponents = [this.topMusic];
 
     this.bindEvents();
+    // 음악을 가져온다
+    await this.fetchMusic();
     this.init();
   }
 
   bindEvents() {
+    // 탭버튼 컴포넌트 이벤트
     this.tabButtons.on("clicktab", (payload) => {
       const { currentIndex = 0 } = payload;
       this.currentMainIndex = currentIndex;
-      this.render;
+      this.render();
     });
+
+    //탑뮤직 컴포넌트 이벤트
+    this.topMusic.on("play", (payload) => {
+      // this.playView.playMusic(payload);
+    });
+    this.topMusic.on("pause", () => {
+      // this.playView.pause();
+    });
+    this.topMusic.on("addPlayList", (payload) => {
+      const { music, musicIndex } = payload;
+      // this.playList.add(music[musicIndex]);
+    });
+  }
+
+  async fetchMusic() {
+    const responseBody = await fetchMusic();
+    const { music = [] } = responseBody;
+    this.topMusic.setMusic(music);
   }
 
   init() {
@@ -36,9 +61,16 @@ export default class App {
     }, 1000);
   }
 
+  renderMainView() {
+    const renderComponent = this.mainViewComponents[this.currentMainIndex];
+    return renderComponent ? renderComponent.render() : "";
+  }
+
   render() {
     removeAllChildNodes(this.rootElement); // 초기화
     const tabButtons = this.tabButtons.render(); // TabButtons를 렌더링하여 버튼들을 가져옴
+    const mainView = this.renderMainView();
     this.rootElement.append(tabButtons); // 루트 요소에 TabButtons를 추가하여 화면에 표시
+    this.rootElement.append(mainView);
   }
 }
---
+++ project04/src/javascripts/components/TopMusic/index.js
@@ -0,0 +1,133 @@
+export default class TopMusic {
+  constructor() {
+    this.rootElement = TopMusic.createRootElement();
+    this.music = []; // 부모로부터 받은 음악 데이터 관리하는 객체
+    this.bindEvents();
+  }
+
+  static createRootElement() {
+    const rootElement = document.createElement("article");
+    rootElement.classList.add("contents-top5");
+
+    return rootElement;
+  }
+
+  bindEvents() {
+    this.rootElement.addEventListener("click", (event) => {
+      const { target } = event.target;
+      const isControllerButton = target.tagName == "BUTTON";
+
+      if (!isControllerButton) {
+        return;
+      }
+
+      const buttonRole = target.classList.item(1); // 버튼 클래스의 두번째 클래스를 갖고 옴
+      switch (buttonRole) {
+        case "icon-play": {
+          this.requestPlay(target);
+          break;
+        }
+        case "icon-pause": {
+          this.requestPause(target);
+          break;
+        }
+        case "icon-plus": {
+          this.requestAddPlayList(target);
+          break;
+        }
+      }
+    });
+  }
+
+  // 모드 음악 재생 상태를 중단하는 UI 변경 처리
+  renderStopAll() {
+    const playingButtons = this.rootElement.querySelectorAll(".icon-pause");
+    playingButtons.forEach((element) => element.classList.replace("icon-pause", "icon-play"));
+  }
+
+  // 음악재생을 App.js에 요청
+  requestPlay(target) {
+    const controller = target.parentElement;
+    const { index: musicIndex } = controller.dataset;
+    const payload = { music: this.music, musicIndex };
+    this.enit("play", payload);
+    this.renderStopAll();
+    target.classList.replace("icon-play", "icon-pause");
+  }
+
+  // 음악 중단을 App.js에 요청
+  requestPause(target) {
+    this.enit("pause");
+    target.classList.replace("icon-pause", "icon-play");
+  }
+
+  // 플레이 리스트에 추가 요청
+  requestAddPlayList(target) {
+    const controller = target.parentElement;
+    const { index: musicIndex } = controller.dataset;
+    const payload = { music: this.music, musicIndex };
+    this.enit("addPlayList", payload);
+  }
+
+  //음악 데이터를 받아오기
+  setMusic(music = []) {
+    this.music = music;
+  }
+
+  // 통신을 받아 처리하는 함수
+  // 다른 코드가 이 클래스에 이벤트와 콜백 함수를 등록할 수 있게 함
+  on(eventName, callback) {
+    // 이벤트 저장소 초기화
+    this.events = this.events ? this.events : {};
+    // 특정 이벤트에 대해 콜백 함수를 저장
+    this.events[eventName] = callback;
+  }
+
+  // 통신을 보내는 함수
+  // 특정 이벤트가 발생했음을 알리고, 필요한 데이터를 전달
+  emit(eventName, payload) {
+    // 해당 이벤트에 연결된 콜백 함수가 있으면 실행
+    this.events[eventName] && this.events[eventName](payload);
+  }
+
+  render() {
+    const topRoof = `
+        <div class="top5-roof">
+            <img src="assets/images/logo.png">
+        </div>
+    `;
+
+    const musicList = this.music
+      .map((music, index) => {
+        const { cover, title, artist } = music;
+        return `
+            <li>
+                <div class="music-rank">${index + 1}</div>
+                <div class="music-content">
+                    <div class="music-cover">
+                        <div class="music-data">
+                            <img src="${cover}" />
+                        </div>
+                        <div class="music-info">
+                            <strong class="music-title">${title}</strong>
+                            <em class="music-artist">${artist[0]}</em>
+                        </div>
+                    </div>
+                    <div class="music-simple-controller" data-index=${index}>
+                        <button class="icon icon-play">
+                            <span class="invisible-text">재생</span>
+                        </button>
+                        <button class="icon icon-plus">
+                            <span class="invisible-text">추가</span>
+                        </button>
+                    </div>
+                </div>
+            </li>
+        `;
+      })
+      .join("");
+    this.rootElement.innerHTML = topRoof + `<ol class="top5-list">` + musicList + `</ol>`;
+
+    return this.rootElement;
+  }
+}
