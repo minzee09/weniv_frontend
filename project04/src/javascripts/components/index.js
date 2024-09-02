e9108f7d74fa4fe0013658dde1869ff45d3ffa13
Minji Kim <minjikim2000@hotmail.com>
2024-09-02 23:31:19 +0900

    refactor: modify folder name

--- project04/src/javascripts/components/index.js
+++ project04/src/javascripts/components/index.js
@@ -1,4 +1,4 @@
-export { default as Intro } from "./intro/index.js";
+export { default as Intro } from "./Intro/index.js";
 export { default as TabButtons } from "./TabButtons/index.js";
 export { default as TopMusic } from "./TopMusic/index.js";
 export { default as SearchView } from "./SearchView/index.js";
